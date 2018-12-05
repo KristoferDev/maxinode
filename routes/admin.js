e4830fc16799d56fb458ed12b1988758d630cd5c
Kristofer <kristofer.stensson@gmail.com>
2018-12-05 16:20:14 +0100

    Fixed broken menu links

--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,10 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-   res.render('/add-product', { pageTitle: 'Add Product', path: '/admin/add-product'});
-});
+   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+ });
 
 router.post('/add-product', (req, res, next) => {
    products.push({ title: req.body.title });
