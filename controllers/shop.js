5fe276db7fd163429112b37e7c6b3e3ddb763c96
GitMac <kristofer.stensson@gmail.com>
2018-12-13 10:28:31 +0100

    changes to views

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 // shop/product-list
 exports.getProducts = ( req, res, next) => {
    Product.fetchAll(products => {
@@ -10,6 +10,17 @@ exports.getProducts = ( req, res, next) => {
       });
    });
 };
+
+exports.getProduct = (req, res, next) => {
+   const prodId = req.params.productId;
+   Product.findById(prodId, product => {
+      res.render('shop/product-detail', {
+         product: product,
+         pageTitle: product.title, 
+         path: '/products'
+      });
+   });
+}
 
 // shop/index
 exports.getIndex = (req, res, next) => {
@@ -27,6 +38,22 @@ exports.getCart = (req, res, next) => {
    res.render('shop/cart', {
       path: '/cart',
       pageTitle: 'Your Cart'
+   });
+}
+
+exports.postCart = (req, res, next) => {
+   const prodId = req.body.productId;
+   Product.findById(prodId, product => {
+      Cart.addProduct(prodId, product.price);
+   });
+   res.redirect('/cart');
+};
+
+// shop/orders
+exports.getOrders = (req, res, next) => {
+   res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders'
    });
 }
 
